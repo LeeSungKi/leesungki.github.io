98efffc65a7f8062a0dbb9e951bfcae788beb6e1
leesungki <leesunggi92@gmail.com>
2021-12-27 16:34:35 +0900

    rss 설정

--- gatsby-config.js
+++ gatsby-config.js
@@ -125,7 +125,61 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-advanced-sitemap`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.nodes.map(node => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  custom_elements: [{ "content:encoded": node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  nodes {
+                    excerpt
+                    html
+                    fields { 
+                      slug 
+                    }
+                    frontmatter {
+                      title
+                      date
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "leesunggi RSS Feed",
+            match: "^/",
+      //      link: "https://feeds.feedburner.com/gatsby/blog",
+          },
+        ],
+      },
+    },
     `gatsby-plugin-image`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
