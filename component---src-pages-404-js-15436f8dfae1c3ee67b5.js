8a36814d75476b87a255899bd8f71e5df30ac97d
aaron <pawer77@gmail.com>
2025-12-21 22:21:59 +0900

    Updates

--- component---src-pages-404-js-15436f8dfae1c3ee67b5.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkleesungki=self.webpackChunkleesungki||[]).push([[883],{6810:function(e,t,n){n.r(t);var u=n(7294),l=n(6096),s=n(5150);t.default=function(){return u.createElement(l.Z,null,u.createElement(s.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-15436f8dfae1c3ee67b5.js.map
